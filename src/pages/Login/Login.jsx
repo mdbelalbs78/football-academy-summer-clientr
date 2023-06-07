34eb9a8b730a9e569557191ad4751303897264cc
belalhossainS <mdbelal01792@gmail.com>
2023-06-07 22:11:12 +0600

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,33 @@
+import { useContext } from 'react';
 import img from '../../assets/fo1.jpg'
+import { AuthContext } from '../../providers/AuthProvider';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+  const {signIn} = useContext(AuthContext)
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password);
+        signIn(email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+        })
+    }
+
     return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">           
-            <img className='mt-10  p-5 ' src={img} alt="" />
+        <div className="hero min-h-screen bg-base-200 ">
+        <div className="hero-content flex-col lg:flex-row-reverse ">
+          <div className="text-center md:w-1/2 lg:text-left">           
+            <img className='mx-5' src={img} alt="" />
           </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+          <div className="card md:w-1/2 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
               <h1 className="text-5xl font-bold">Login now!</h1>
                 <label className="label">
@@ -20,7 +39,7 @@ const Login = () => {
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
+                <input type="password" name='password' placeholder="password" className="input input-bordered" />
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
@@ -30,6 +49,7 @@ const Login = () => {
                 <input className="btn btn-primary" type="submit" value="Login" />
               </div>
             </form>
+            <p><small>Now Here <Link to='/signup'>Please sign up</Link></small></p>
           </div>
         </div>
       </div>
