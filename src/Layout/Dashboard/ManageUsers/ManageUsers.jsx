d1f3138d928ae0424eed268d81f254824ade786f
belalhossainS <mdbelal01792@gmail.com>
2023-06-11 15:06:36 +0600

    acces token

--- src/Layout/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/Layout/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import { FaTrashAlt } from "react-icons/fa";
+import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const ManageUsers = () => {
     const {data:users = [], refetch} =useQuery(['users'], async() => {
@@ -9,6 +10,25 @@ const ManageUsers = () => {
 
     const handleDelete = user => {
 
+    }
+
+    const handleMakeAdmin = user => {
+       fetch(`http://localhost:5000/users/admin/${user._id}`, {
+         method: 'PATCH',
+       })
+       .then(res => res.json())
+       .then(data => {
+        if(data.modifiedCount){
+            refetch();
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: `${user.name} is an Admin `,
+                showConfirmButton: false,
+                timer: 1500
+              })
+        }
+       })
     }
 
     return (
@@ -37,7 +57,12 @@ const ManageUsers = () => {
             <td>{user.email}</td>         
             <td>Role</td>
             <td><button className="bg-yellow-500 btn btn-ghost">in</button></td>
-            <td>Role</td>
+            <td>
+                {
+                    user.role === 'admin' ? 'admin' : 
+                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost  bg-yellow-500"><FaUserShield></FaUserShield></button>
+                }
+            </td>
             <td>
               <button onClick={() => handleDelete(user)} className="btn btn-ghost  bg-red-500"><FaTrashAlt></FaTrashAlt></button>
             </td>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 
 
 
@@ -45,6 +46,19 @@ const AuthProvider = ({children}) => {
        const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
             console.log('current user', currentUser);
+            
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+                .then(data =>{
+                    localStorage.setItem('access-token', data.data.token)
+                    setLoading(false);
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
+
+
             setLoading(false)
         })
         return () => {
